897c0cd80615b854fd607a1e0f40cf4fb686baa9
vikashivar <vikashivar246@gmail.com>
2023-06-23 17:35:25 +0530

    second

--- src/setCom/section1.js
+++ src/setCom/section1.js
@@ -2,71 +2,221 @@ import React from "react";
 import sp1 from "./picture/Group 161.png";
 import sp2 from "./picture/info_1_a39178e895 (1).png";
 import sp3 from "./picture/Group.svg";
+import sp4 from "./picture/Frame_17145_7923a090c0.jpg";
+import sp5 from "./picture/Group.png";
+import sp6 from "./picture/Ellipse 21.png";
+import sp7 from "./picture/service_Provider_3c32c97b29.png";
+import sp8 from "./picture/Group 158.png";
 
 function Section1() {
   return (
-    <div
-      className="d-flex justify-content-between "
-      style={{ margin: "6rem 6rem 0rem 6rem" }}
-    >
-      <div>
+    <>
+      <div
+        className="d-flex justify-content-between "
+        style={{ margin: "6rem 6rem 0rem 6rem" }}
+      >
         <div>
+          <div>
+            <div
+              style={{
+                color: "#393939",
+                fontSize: "3.125rem",
+                fontFamily: "Geomanist,sans-serif",
+                width: "34.125rem",
+              }}
+            >
+              Access to choice in global finance is limited
+            </div>
+            <div
+              style={{
+                color: "#4E59E7",
+                fontSize: "50px",
+                fontFamily: "Geomanist,sans-serif",
+                fontWeight: 700,
+              }}
+            >
+              Not anymore.
+            </div>
+          </div>
+          <div style={{ marginTop: "7rem" }}>
+            <img
+              src={sp2}
+              style={{ width: "25.375rem", height: "30.75rem" }}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="mt-5" style={{}}>
           <div
             style={{
-              color: "#393939",
-              fontSize: "3.125rem",
+              width: "4.5rem",
+              height: "0.5rem",
+              borderRadius: "8px",
+              background: "#4e59e7",
+              marginLeft: "7rem",
+            }}
+          ></div>
+          <div
+            className="mt-5"
+            style={{
+              color: "#8F8F98",
+              fontSize: "1.125rem",
               fontFamily: "Geomanist,sans-serif",
-              width: "34.125rem",
+              lineHeight: "1.875rem",
+              paddingLeft: "7rem",
             }}
           >
-            Access to choice in global finance is limited
+            Banks lock you in a silo when they offer their accounts, payments,
+            cards, insurance and loans products without access to choice of
+            products by other financial service providers. Not any more.
+          </div>
+          <div style={{ marginTop: "10rem" }}>
+            <img
+              src={sp1}
+              style={{ width: "4.812rem", height: "4.734rem" }}
+            ></img>
+
+            <div
+              className="mt-5"
+              style={{
+                color: "rgba(142, 95, 245, 1)",
+                fontSize: "2.5rem",
+                fontFamily: "Geomanist,sans-serif",
+                fontWeight: "600",
+              }}
+            >
+              <span style={{ color: "#393939", fontWeight: "400" }}>For </span>
+              Indivisuals
+            </div>
+            <div
+              style={{
+                color: "#393939",
+                fontSize: "1.125rem",
+                fontFamily: "Geomanist,sans-serif",
+                lineHeight: "1.875rem",
+              }}
+            >
+              Choose how you pay, invest and trade
+            </div>
+            <div
+              className="mt-5 pe-5"
+              style={{
+                color: "#8F8F98",
+                fontSize: "1.125rem",
+                fontFamily: "Geomanist,sans-serif",
+                lineHeight: "1.875rem",
+              }}
+            >
+              Individuals can set up accounts on the Verified Network for
+              keeping assets, make payments in multiple currencies, invest and
+              trade digital assets.
+            </div>
+          </div>
+          <div className="mt-5">
+            <a
+              href="/"
+              style={{
+                textDecoration: "none",
+                color: "#8E5FF5",
+                fontSize: "1.125rem",
+                fontFamily: "Geomanist,sans-serif",
+                lineHeight: "1.625rem",
+              }}
+            >
+              Learn More
+              <img
+                src={sp3}
+                style={{
+                  width: "0.93rem",
+                  height: "0.72rem",
+                  marginLeft: "0.55rem",
+                }}
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/*  ---------------------------------------------------------- */}
+      <div className="d-flex" style={{ margin: "10em 6rem 0rem 6rem" }}>
+        <div style={{}}>
+          <div className="position-relative">
+            <img
+              src={sp6}
+              style={{ width: "3.60rem", height: "3.60rem ," }}
+              alt=""
+            />
+            <img
+              src={sp5}
+              style={{
+                width: "4.038rem",
+                height: "3.949rem ,",
+                position: "absolute",
+                left: "1em",
+                top: "15px",
+              }}
+            ></img>
+          </div>
+
+          <div
+            className="mt-5"
+            style={{
+              color: "#08C0B5",
+              fontSize: "2.5rem",
+              fontFamily: "Geomanist,sans-serif",
+              fontWeight: "600",
+            }}
+          >
+            <span style={{ color: "#393939", fontWeight: "400" }}>For </span>
+            Businesses
           </div>
           <div
             style={{
-              color: "#4E59E7",
-              fontSize: "50px",
+              color: "#393939",
+              fontSize: "1.125rem",
               fontFamily: "Geomanist,sans-serif",
-              fontWeight: 700,
+              lineHeight: "1.875rem",
             }}
           >
-            Not anymore.
+            Choose how you finance your business.
+          </div>
+          <div
+            className="mt-5 pe-5"
+            style={{
+              color: "#08C0B5",
+              fontSize: "1.125rem",
+              fontFamily: "Geomanist,sans-serif",
+              lineHeight: "1.875rem",
+            }}
+          >
+            Businesses can set up accounts on the Verified Network, make
+            payments and raise financing by issuing regulation compliant
+            tokenized securities.
           </div>
         </div>
         <div style={{ marginTop: "7rem" }}>
           <img
-            src={sp2}
-            style={{ width: "25.375rem", height: "30.75rem" }}
+            src={sp4}
+            style={{ width: "28.81rem", height: "29.743rem" }}
             alt=""
           />
         </div>
       </div>
-      <div className="mt-5" style={{}}>
-        <div
-          style={{
-            width: "4.5rem",
-            height: "0.5rem",
-            borderRadius: "8px",
-            background: "#4e59e7",
-            marginLeft: "7rem",
-          }}
-        ></div>
-        <div
-          className="mt-5"
-          style={{
-            color: "#8F8F98",
-            fontSize: "1.125rem",
-            fontFamily: "Geomanist,sans-serif",
-            lineHeight: "1.875rem",
-            paddingLeft: "7rem",
-          }}
-        >
-          Banks lock you in a silo when they offer their accounts, payments,
-          cards, insurance and loans products without access to choice of
-          products by other financial service providers. Not any more.
-        </div>
-        <div style={{ marginTop: "10rem" }}>
+
+      <div
+        className="d-flex justify-content-between"
+        style={{ marginTop: "7rem", margin: "10em 8rem 0rem 8rem" }}
+      >
+        <img
+          src={sp7}
+          style={{ width: "28.43rem", height: "27.75rem" }}
+          alt=""
+        />
+
+        <div style={{}}>
           <img
-            src={sp1}
+            src={sp8}
             style={{ width: "4.812rem", height: "4.734rem" }}
           ></img>
 
@@ -80,7 +230,7 @@ function Section1() {
             }}
           >
             <span style={{ color: "#393939", fontWeight: "400" }}>For </span>
-            Indivisuals
+            Service Providers
           </div>
           <div
             style={{
@@ -90,7 +240,7 @@ function Section1() {
               lineHeight: "1.875rem",
             }}
           >
-            Choose how you pay, invest and trade
+            Get access to customers and markets.
           </div>
           <div
             className="mt-5 pe-5"
@@ -101,36 +251,12 @@ function Section1() {
               lineHeight: "1.875rem",
             }}
           >
-            Individuals can set up accounts on the Verified Network for keeping
-            assets, make payments in multiple currencies, invest and trade
-            digital assets.
+            Financial Services Providers on the Verified Network get access to
+            individual and business customers to whom they can offer services.
           </div>
         </div>
-        <div className="mt-5">
-          <a
-            href="/"
-            style={{
-              textDecoration: "none",
-              color: "#8E5FF5",
-              fontSize: "1.125rem",
-              fontFamily: "Geomanist,sans-serif",
-              lineHeight: "1.625rem",
-            }}
-          >
-            Learn More
-            <img
-              src={sp3}
-              style={{
-                width: "0.93rem",
-                height: "0.72rem",
-                marginLeft: "0.55rem",
-              }}
-              alt=""
-            />
-          </a>
-        </div>
       </div>
-    </div>
+    </>
   );
 }
 
